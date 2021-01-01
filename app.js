847e8ed59fdab1eb7aa251aa631e5fa4b634253c
Todd Ransom <toddransom@me.com>
2021-01-01 13:16:13 -0500

    v0.5.0

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var topicRouter = require('./routes/topic');
@@ -10,12 +11,8 @@ var topicRouter = require('./routes/topic');
 var app = express();
 var cors = require('cors')
 
-var current_value = {}
-var last_update = {}
-// TODO multiple topic support 
-// { topic1: { last_update: "date string", current_value: "some string" },
-// topic2: {} }
-
+var current_value = {};
+var last_update = {};
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,45 +20,36 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
-var mqtt_client = require('./data/mqtt-topics')
+var mqtt_client = require('./data/mqtt-topics');
 mqtt_client.on('message', function (topic, message) {
-  let date_ob = new Date()
-  // adjust 0 before single digit date
-  let date = ("0" + date_ob.getDate()).slice(-2)
-  let month = ("0" + (date_ob.getMonth() + 1)).slice(-2)
-  let year = date_ob.getFullYear()
-  let hours = date_ob.getHours()
-  let minutes = date_ob.getMinutes()
-  let seconds = date_ob.getSeconds()
-  // prints date & time in YYYY-MM-DD HH:MM:SS format
-  let date_string = year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds
+  const date = new Date().getTime();
 
   if ( message.toString() !== current_value ) {
     const re = /\//gi;
-    const topic_with_subs = topic.replace(re, '-')
-    console.log("%s;%s;%s", date_string, topic_with_subs, message.toString());
+    const topic_with_subs = topic.replace(re, '-');
+    console.log("%s;%s;%s", date, topic_with_subs, message.toString());
     current_value[topic_with_subs] = message.toString();
-    last_update[topic_with_subs] = date_string;
+    last_update[topic_with_subs] = `${date}`;
   }
-}) 
+});
 
 app.get('/topic/:topicname/current', (req,res) => {
   res.json({
     topic: req.params.topicname,
     value: current_value[req.params.topicname],
     lastUpdated: last_update[req.params.topicname]
-  })
+  });
 });
 
 app.get('/topic/:topicname/past/:unit/:quantity', (req,res) => {
-  res.send("topic past")
+  res.send("topic past");
 });
 
 // catch 404 and forward to error handler
--- data/mqtt-topics.js
+++ data/mqtt-topics.js
@@ -1,25 +1,25 @@
-var mqtt = require('mqtt')
-var client  = mqtt.connect('mqtt://10.0.0.200')
+var mqtt = require('mqtt');
+var client  = mqtt.connect(process.env.MQTT_URL);
 
 client.on('connect', function () {
   // TODO read topics from config.json
   client.subscribe('bus/1/temp', function (err) {
     if (err) {
-      console.log(err)
-      console.log("ending mqtt session")
-      client.end()
+      console.log(err);
+      console.log("ending mqtt session");
+      client.end();
     }
-  })
+  });
 
 
   client.subscribe('bus/1/humidity', function (err) {
     if (err) {
-      console.log(err)
-      console.log("ending mqtt session")
-      client.end()
+      console.log(err);
+      console.log("ending mqtt session");
+      client.end();
     }
-  })
-})
+  });
+});
 
 
-module.exports = client
+module.exports = client;
